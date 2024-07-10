05583ae882d1df03ef71a21487964ccd53f72944
MellowMeh <123280572+MellowMeh@users.noreply.github.com>
2024-07-09 21:26:24 -0400

    Add form inputs

--- script/index.js
+++ script/index.js
@@ -20,31 +20,57 @@ function addBookToLibrary() {
     addBookButton.addEventListener('click', () => {
         let card = document.createElement('div');
             card.setAttribute('class', 'card');
+
         let titleEntryArea = document.createElement('div');
             titleEntryArea.setAttribute('class', 'entry-area');
         let authorEntryArea = document.createElement('div');
             authorEntryArea.setAttribute('class', 'entry-area');
+        let pagesEntryArea = document.createElement('div');
+            pagesEntryArea.setAttribute('class', 'entry-area')
+        let readEntryArea = document.createElement('div');
+            readEntryArea.setAttribute('class', 'entry-area');
+
         let titleInput = document.createElement('input');
-            titleInput.setAttribute('class', 'title-input');
             titleInput.setAttribute('type', 'text');
             titleInput.required = true;
         let titleLabelline = document.createElement('div');
             titleLabelline.setAttribute('class', 'labelline');
             titleLabelline.textContent = 'book title'
+
         let authorInput = document.createElement('input');
-            authorInput.setAttribute('class', 'title-input');
             authorInput.setAttribute('type', 'text');
             authorInput.required = true;
         let authorLabelline = document.createElement('div');
             authorLabelline.setAttribute('class', 'labelline');
             authorLabelline.textContent = 'author name'
+
+        let pagesInput = document.createElement('input');
+            pagesInput.setAttribute('type', 'text');
+            pagesInput.required = true;
+        let pagesLabelline = document.createElement('div');
+            pagesLabelline.setAttribute('class', 'labelline');
+            pagesLabelline.textContent = 'number of pages'
+
+        let readInput = document.createElement('input');
+            readInput.setAttribute('type', 'text');
+            readInput.required = true;
+        let readLabelline = document.createElement('div');
+            readLabelline.setAttribute('class', 'labelline');
+            readLabelline.textContent = 'read (y/n)'
+
         cardContainer.appendChild(card);
-        card.appendChild(titleEntryArea)
-        titleEntryArea.appendChild(titleInput);
-        titleEntryArea.appendChild(titleLabelline);
-        card.appendChild(authorEntryArea);
-        authorEntryArea.appendChild(authorInput);
-        authorEntryArea.appendChild(authorLabelline);
+            card.appendChild(titleEntryArea)
+                titleEntryArea.appendChild(titleInput);
+                titleEntryArea.appendChild(titleLabelline);
+            card.appendChild(authorEntryArea);
+                authorEntryArea.appendChild(authorInput);
+                authorEntryArea.appendChild(authorLabelline);
+            card.appendChild(pagesEntryArea);
+                pagesEntryArea.appendChild(pagesInput);
+                pagesEntryArea.appendChild(pagesLabelline);
+            card.appendChild(readEntryArea);
+                readEntryArea.appendChild(readInput);
+                readEntryArea.appendChild(readLabelline);
     })
 }
 
